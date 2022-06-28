784c5cb7e89f3af79ae991c4437fde96b5a4245d
Елена Николаева <elena@192.168.0.10>
2022-06-28 15:27:56 +0300

    Example010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿//             0   1   2  3    4   5   6   7   8
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1; Программистами принято 
+    //писать в более короткой форме
+    index++;
+}
