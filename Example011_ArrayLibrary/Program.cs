4422d0647571d15fe4dd715f024e091964c0e4bc
Елена Николаева <elena@192.168.0.10>
2022-06-28 22:11:03 +0300

    Example011 ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,63 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
+
+// int [] array = Звучит это так:
+// Начнём с того, что определим новый МАССИВ
+// название, которому дадим array.
+
+// new int[10]; Звучит это так: 
+// Создай новый массив, в котором будет, например, 10 элементов.
+// Есть методы, которые возвращают какие-то значения, а
+// void (войт метод) - это метод, который ничего не возвращает.
+// В коде мы тогда не должны будем использовать оператор return,
+// который был, например, для поиска максимума из трёх.
+// col, коротко от слова collection
