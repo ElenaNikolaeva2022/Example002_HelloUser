5ffd2d78e90ccdf9eb06e30184f249a7fa6e03c4
Елена Николаева <elena@192.168.0.10>
2022-07-06 14:36:15 +0300

    Example004_Div коррект

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,10 @@
-﻿double numberA = 12;
+﻿// int (integer) - контейнер,который позволяет работать с целыми числами 2
+// int numberA = 10;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+// double (дабл) - контейнер,который позволяет работать с вещественными числами 2,4
+
+double numberA = 12;
 double numberB = 5;
 Console.WriteLine(numberA / numberB);
