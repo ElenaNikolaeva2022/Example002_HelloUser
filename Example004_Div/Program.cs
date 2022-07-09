3f605a2a2f4a7564a7f3d416fac28d8088c3f0ee
Елена Николаева <elena@192.168.0.10>
2022-07-09 09:13:00 +0300

    Добавлены решения

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,10 +1,11 @@
-﻿// int (integer) - контейнер,который позволяет работать с целыми числами 2
+﻿// Вариант 1.
+// int (integer) - контейнер,который позволяет работать с целыми числами 2:
 // int numberA = 10;
 // int numberB = 5;
 // Console.WriteLine(numberA / numberB);
 
-// double (дабл) - контейнер,который позволяет работать с вещественными числами 2,4
-
+// Вариант 2.
+// double (дабл) - контейнер,который позволяет работать с вещественными числами 2,4:
 double numberA = 12;
 double numberB = 5;
 Console.WriteLine(numberA / numberB);
